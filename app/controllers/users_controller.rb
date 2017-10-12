02d0b8e619ab87435e5efcbee2cadec26ed7cfb8
Vicente Rotman <vrotmanh@gmail.com>
2017-10-12 00:52:44 -0400

    Create Login Api request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+
+    def login
+			user = User.find_by(email: params[:email])
+			if user && user.authenticate(params[:password])
+			  render json: {id: user.id}, status: :ok
+			else
+			  render json: {error: 'Wrong email or password'}, status: :unauthorized
+		  end
+    end
 end
