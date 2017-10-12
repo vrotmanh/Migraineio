306b3ded75b288c266a61e08f83b4f4a9f33f4a4
Vicente Rotman <vrotmanh@gmail.com>
2017-10-12 01:45:00 -0400

    Create Register Api request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
 			  render json: {error: 'Wrong email or password'}, status: :unauthorized
 		  end
     end
+
+	def create
+		if params[:email] and params[:name] and params[:password] and params[:kind]
+			user = User.create(email: params[:email], name: params[:name], password: params[:password], password_confirmation: params[:password], kind: params[:kind])
+			puts user.errors
+			if user.errors.empty?
+				render json: {id: user.id}, status: :created
+			else
+				render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+			end
+		else
+			render json: {error: user.errors.full_messages}, status: :unprocessable_entity
+		end
+  end
 end
